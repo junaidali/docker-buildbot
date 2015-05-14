b7e13f38cdfed0472b770aed8bb9d0a2856cf41c
Junaid Ali <mailtojunaid@gmail.com>
2015-05-14 15:38:39 +0000

    Using environment variables to set different project parameters

---
+++ master/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Replace environment variables
+sed -i 's/\"pyflakes\"\,\"pyflakes\"/'\""$ADMIN_USERNAME"\"\,\""$ADMIN_PASSWORD"\"'/' /usr/src/app/master/master.cfg
+sed -i 's/\"example-slave\"\, \"pass\"/'\""$SLAVE_USERNAME"\"\, \""$SLAVE_PASSWORD"\"'/' /usr/src/app/master/master.cfg
+sed -i 's/Pyflakes/'"$PROJECT_TITLE"'/' /usr/src/app/master/master.cfg
+sed -i 's/https\:\/\/launchpad\.net\/pyflakes\/'"$PROJECT_URL"'/' /usr/src/app/master/master.cfg
+
+# Run build-master
+twistd --nodaemon --no_save -y /usr/src/app/master/buildbot.tac
--- slave/run.sh
+++ slave/run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 # Replace environment variables
 sed -i 's/localhost/'"$MASTER_PORT_8010_TCP_ADDR"'/' /usr/src/app/slave/buildbot.tac
+sed -i 's/example-slave/'"$MASTER_ENV_SLAVE_USERNAME"'/' /usr/src/app/slave/buildbot.tac
+sed -i 's/'"passwd \= \'pass\'"'/passwd \= '\'"$MASTER_ENV__SLAVE_PASSWORD"\''/' /usr/src/app/slave/buildbot.tac
+
+echo $SLAVE_DESCRIPTION > /usr/src/app/slave/info/host
+echo $SLAVE_ADMINISTRATOR > /usr/src/app/slave/info/admin
 
 # Run build-slave
 twistd --nodaemon --no_save -y /usr/src/app/slave/buildbot.tac
