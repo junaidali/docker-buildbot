e73e088d32a659a3128092bb9da7a3b0b59d174e
Junaid Ali <alisyed8@iit.edu>
2015-05-19 15:23:13 -0500

    Add SSH Agent support for master. This will allow for polling git repositories that need authentication

--- master/run.sh
+++ master/run.sh
@@ -6,5 +6,21 @@ sed -i 's/Pyflakes/'"$PROJECT_TITLE"'/' /usr/src/app/master/master.cfg
 sed -i 's,https://launchpad.net/pyflakes,'"$PROJECT_URL"',g' /usr/src/app/master/master.cfg
 sed -i 's,git://github.com/junaidali/pyflakes.git,'"$GIT_REPO_URL"',g' /usr/src/app/master/master.cfg
 
+# Start SSH-Agent
+echo "Starting ssh agent"
+eval `ssh-agent`
+
+# Add all SSH keys within .ssh/. Make sure they don't have a password.
+for key in `find .ssh/ -type f`
+do
+	if [ -f $key ]; then
+		if [ ${key: -4} == ".pub" ]; then
+			echo "Ignoring public key $key"
+		else
+			echo "Adding private key $key to ssh-agent"
+			ssh-add $key
+		fi
+	fi
+done
 # Run build-master
 twistd --nodaemon --no_save -y /usr/src/app/master/buildbot.tac
